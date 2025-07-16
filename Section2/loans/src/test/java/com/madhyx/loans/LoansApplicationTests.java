07eca92f8ce6d5f560932acaaf89e28d2b6e12f1
Shaikh Zaid <szaid07@gmail.com>
2025-07-16 08:02:57 +0400

    Added Loans and Cards Modules

---
+++ Section2/loans/src/test/java/com/madhyx/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhyx.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
